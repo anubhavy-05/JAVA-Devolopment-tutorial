889a6bf97d22309134ab3ec7e20a3b29c26144e4
anubhavy-05 <ay8400673@gmail.com>
2025-11-02 23:13:21 +0530

    Add main method to variable4 class with final variable example

--- LECTURE/Lecture1/Lecture2/variable4.java
+++ LECTURE/Lecture1/Lecture2/variable4.java
@@ -1,3 +1,8 @@
 public class variable4 {
-    
+    public static void main(String[] args) {
+        final int MAX_VALUE = 100;
+        System.out.println("MAX_VALUE = " + MAX_VALUE);
+
+        // MAX_VALUE = 200; // ❌ Error: cannot assign a value to final variable
+    }
 }
