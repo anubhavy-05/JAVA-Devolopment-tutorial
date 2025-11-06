3720b9e0d2c1aa35d0faac430724471a9e48a97d
anubhavy-05 <ay8400673@gmail.com>
2025-11-06 21:06:18 +0530

    Add QuestionNum1 class with main method to demonstrate basic arithmetic

---
+++ LECTURE/Lecture1/Lecture3/solution/QuistionNum1.java
@@ -0,0 +1,14 @@
+public class QuestionNum1 {
+    public static void main(String[] args) {
+        // Declare three numbers
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        // Add the numbers
+        int sum = num1 + num2 + num3;
+
+        // Print the result
+        System.out.println("The sum of three numbers is: " + sum);
+    }
+}
