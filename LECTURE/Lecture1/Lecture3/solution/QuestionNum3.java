d793f071f62966c503cd95d3efbc7da1976ca396
anubhavy-05 <ay8400673@gmail.com>
2025-11-07 22:38:37 +0530

    Implement main method in QuestionNum3 class to greet user by name

--- LECTURE/Lecture1/Lecture3/solution/QuestionNum3.java
+++ LECTURE/Lecture1/Lecture3/solution/QuestionNum3.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;  // Import Scanner class to take input
+
 public class QuestionNum3 {
-    
+    public static void main(String[] args) {
+        // Create Scanner object to read input
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user for their name
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();  // Store user input
+
+        // Print greeting message
+        System.out.println("Hello " + name + ", have a good day!");
+
+        // Close the scanner to free memory
+        sc.close();
+    }
 }
